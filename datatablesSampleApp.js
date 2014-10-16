42e0cbe15de31cf9af3fb919024f6a18a8836d43
Rafael Blázquez <rafael.blazquez@tuguu.com>
2014-10-16 16:05:43 +0100

    Datatble feeded from json string

--- datatablesSampleApp.js
+++ datatablesSampleApp.js
@@ -1,1 +1,27 @@
-angular.module('datatablesSampleApp', ['datatables']);
+angular.module('datatablesSampleApp', ['datatables']).controller('withAjaxCtrl', function ($scope, DTOptionsBuilder, DTColumnBuilder) {
+    $scope.dtOptions = {
+    	paginationType: 'full_numbers',
+    	 "data": [{
+			    "id": 860,
+			    "firstName": "Superman",
+			    "lastName": "Yoda"
+			}, {
+			    "id": 870,
+			    "firstName": "Foo",
+			    "lastName": "Whateveryournameis"
+			}, {
+			    "id": 590,
+			    "firstName": "Toto",
+			    "lastName": "Titi"
+			}, {
+			    "id": 803,
+			    "firstName": "Luke",
+			    "lastName": "Kyle"
+		}]
+	};
+    $scope.dtColumns = [
+        DTColumnBuilder.newColumn('id').withTitle('ID'),
+        DTColumnBuilder.newColumn('firstName').withTitle('First name'),
+        DTColumnBuilder.newColumn('lastName').withTitle('Last name').notVisible()
+    ];
+});
